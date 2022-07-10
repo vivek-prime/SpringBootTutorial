4591f02fcaafeba178e1d923a3d849520dce27e1
vivek-prime <vivek-prime@github.com>
2022-07-10 17:44:04 +0530

    first commit

---
+++ src/test/java/com/example/SpringBootTutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
