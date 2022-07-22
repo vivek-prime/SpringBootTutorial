66143cc833e81991e03cfdddb33f5541e038f903
vivek-prime <vivek-prime@github.com>
2022-07-22 19:07:23 +0530

    HelloController added

---
+++ src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.SpringBootTutorial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String helloWorld(){
+        return "Welcome to Learning";
+    }
+
+}
