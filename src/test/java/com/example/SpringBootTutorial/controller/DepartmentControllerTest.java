b20f3adfd296f9862a47ca43c37d3a5bddb4229c
vivek <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>
2022-07-24 15:33:33 +0530

    test class for controller added

---
+++ src/test/java/com/example/SpringBootTutorial/controller/DepartmentControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.SpringBootTutorial.controller;
+
+import com.example.SpringBootTutorial.entity.Department;
+import com.example.SpringBootTutorial.service.DepartmentService;
+import com.example.SpringBootTutorial.service.DepartmentServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
+    @MockBean
+    private DepartmentService departmentService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Department department;
+
+    @BeforeEach
+    void setUp() {
+        department = Department.builder()
+                .deptId(1L)
+                .deptName("CSE")
+                .deptAddress("Kolkata")
+                .deptCode("CSE-100")
+                .build();
+        Mockito.when(departmentService.saveDept(Mockito.any(Department.class))).thenReturn(department);
+    }
+
+    @Test
+    void saveDept() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"deptName\": \"CSE\",\n" +
+                        "    \"deptAddress\": \"Kolkata\",\n" +
+                        "    \"deptCode\": \"CSE-100\"\n" +
+                        "  }"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
