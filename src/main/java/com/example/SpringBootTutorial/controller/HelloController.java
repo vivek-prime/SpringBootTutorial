ca64e1a6d316d9672ce5eef9340fb494dcf74163
vivek <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>
2022-07-24 15:41:13 +0530

    using app properties to print welcome message

--- src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
+++ src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.example.SpringBootTutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String helloWorld(){
-        return "Welcome to Learning Mr. Vivek!!!!!!!!!1";
+        return welcomeMessage;
     }
 
 }
