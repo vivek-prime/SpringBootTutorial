01261d7dbfae450a8463a9ea7c4e9eb16d74b37b
vivek-prime <vivek.400428@gmail.com>
2022-07-28 01:30:11 +0530

    playing with config properties

--- src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
+++ src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
@@ -5,14 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
     @Value("${app.welcome.message}")
     String welcomeMessage;
 
+    @Value("${app.list.values}")
+    List<String> listValues;
+
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String helloWorld(){
-        return welcomeMessage;
+        return welcomeMessage + " - " + listValues;
     }
 
 }
