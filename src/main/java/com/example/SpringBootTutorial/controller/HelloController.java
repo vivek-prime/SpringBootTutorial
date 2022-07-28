f6c8083fbccc810dd61ed0d80f3d290776b96785
vivek-prime <vivek.400428@gmail.com>
2022-07-29 02:11:53 +0530

    using RequestParam

--- src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
+++ src/main/java/com/example/SpringBootTutorial/controller/HelloController.java
@@ -4,9 +4,10 @@ import com.example.SpringBootTutorial.config.DbConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @Slf4j
@@ -21,10 +22,14 @@ public class HelloController {
     @Autowired
     private DbConfig dbConfig;
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public String helloWorld(){
+    @GetMapping(value = "/")
+    public String helloWorld() {
         log.info("dbconfig - {}", dbConfig);
         return welcomeMessage + " - " + listValues;
     }
 
+    @GetMapping("/getList")
+    public String getList(@RequestParam List<String> ids) {
+        return "IDS are - " + ids;
+    }
 }
