39bed3fb127f2903f3e36d5365a380b52fdbb80e
vivek-prime <vivek.400428@gmail.com>
2022-07-29 02:12:26 +0530

    check for Exception thrown in test method

--- src/test/java/com/example/SpringBootTutorial/utlities/MyDictionaryTest.java
+++ src/test/java/com/example/SpringBootTutorial/utlities/MyDictionaryTest.java
@@ -1,9 +1,10 @@
 package com.example.SpringBootTutorial.utlities;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -11,16 +12,20 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MyDictionaryTest {
+    private MyDictionary dictMock;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
+        log.info("Tests are going to start");
+        dictMock = mock(MyDictionary.class);
     }
 
     @Test()
     @DisplayName("Throw Null Pointer Exception when key is not found")
     public void throwException() {
-        MyDictionary dictMock = mock(MyDictionary.class);
+
         when(dictMock.getMeaning(anyString())).thenThrow(NullPointerException.class);
         assertThrows(NullPointerException.class, () -> dictMock.getMeaning("word"), "Key not found");
     }
